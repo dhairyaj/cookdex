702a2e644aad605cb06ab1a2f340c189651d9441
dhairyaj <dhairya.jain.work@gmail.com>
2023-01-11 00:57:03 +0530

    Add animation using framer-motion

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Home from './Home'
 import Cuisine from './Cuisine';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SearchedItem from './SearchedItem';
 import RecipieDetails from './RecipieDetails';
+import { AnimatePresence } from 'framer-motion';
 
 function Pages() {
+  const location = useLocation();
   return (
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/cuisine/:type" element={<Cuisine />}/>
+    <AnimatePresence exitBeforeEnter>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
         <Route path="/find/:search" element={<SearchedItem />} />
         <Route path="/recipe/:id" element={<RecipieDetails />} />
       </Routes>
+    </AnimatePresence>
   )
 }
 
